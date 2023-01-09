17211ae49b539d2a3ed9ab16710a830382465aa4
kkader568 <117683636+kkader568@users.noreply.github.com>
2023-01-09 19:49:40 +0000

    test codespace

--- src/Display.js
+++ src/Display.js
@@ -14,7 +14,7 @@ const GroupDisplay = (props) => {
   for(let i = 0; i < props.inputData.length; i++){
     let groupNumInt = i+1;
     let groupNumString = groupNumInt.toString();
-    let header = "Group " + groupNumString; 
+    let header = "Grou " + groupNumString; 
     dataDict[header] = props.inputData[i];
   }
   let data = [dataDict];
