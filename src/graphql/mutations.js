4103a4de973ea00d13cfd1938f43195bcfb5ace1
techforteachers <brandonzion.sun@gmail.com>
2023-02-19 15:14:13 -0500

    deleted user attribute from GraphQL

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -12,8 +12,20 @@ export const createStudent = /* GraphQL */ `
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
@@ -28,8 +40,20 @@ export const updateStudent = /* GraphQL */ `
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
@@ -44,8 +68,215 @@ export const deleteStudent = /* GraphQL */ `
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
+    }
+  }
+`;
+export const createClass = /* GraphQL */ `
+  mutation CreateClass(
+    $input: CreateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    createClass(input: $input, condition: $condition) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateClass = /* GraphQL */ `
+  mutation UpdateClass(
+    $input: UpdateClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    updateClass(input: $input, condition: $condition) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteClass = /* GraphQL */ `
+  mutation DeleteClass(
+    $input: DeleteClassInput!
+    $condition: ModelClassConditionInput
+  ) {
+    deleteClass(input: $input, condition: $condition) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const createClassStudent = /* GraphQL */ `
+  mutation CreateClassStudent(
+    $input: CreateClassStudentInput!
+    $condition: ModelClassStudentConditionInput
+  ) {
+    createClassStudent(input: $input, condition: $condition) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const updateClassStudent = /* GraphQL */ `
+  mutation UpdateClassStudent(
+    $input: UpdateClassStudentInput!
+    $condition: ModelClassStudentConditionInput
+  ) {
+    updateClassStudent(input: $input, condition: $condition) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const deleteClassStudent = /* GraphQL */ `
+  mutation DeleteClassStudent(
+    $input: DeleteClassStudentInput!
+    $condition: ModelClassStudentConditionInput
+  ) {
+    deleteClassStudent(input: $input, condition: $condition) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -2,41 +2,281 @@
 // this is an auto generated file. This will be overwritten
 
 export const onCreateStudent = /* GraphQL */ `
-  subscription OnCreateStudent($filter: ModelSubscriptionStudentFilterInput) {
-    onCreateStudent(filter: $filter) {
+  subscription OnCreateStudent(
+    $filter: ModelSubscriptionStudentFilterInput
+    $owner: String
+  ) {
+    onCreateStudent(filter: $filter, owner: $owner) {
       id
       first_name
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
 export const onUpdateStudent = /* GraphQL */ `
-  subscription OnUpdateStudent($filter: ModelSubscriptionStudentFilterInput) {
-    onUpdateStudent(filter: $filter) {
+  subscription OnUpdateStudent(
+    $filter: ModelSubscriptionStudentFilterInput
+    $owner: String
+  ) {
+    onUpdateStudent(filter: $filter, owner: $owner) {
       id
       first_name
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
     }
   }
 `;
 export const onDeleteStudent = /* GraphQL */ `
-  subscription OnDeleteStudent($filter: ModelSubscriptionStudentFilterInput) {
-    onDeleteStudent(filter: $filter) {
+  subscription OnDeleteStudent(
+    $filter: ModelSubscriptionStudentFilterInput
+    $owner: String
+  ) {
+    onDeleteStudent(filter: $filter, owner: $owner) {
       id
       first_name
       last_name
       grade
       user
+      classes {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
       createdAt
       updatedAt
+      owner
+    }
+  }
+`;
+export const onCreateClass = /* GraphQL */ `
+  subscription OnCreateClass(
+    $filter: ModelSubscriptionClassFilterInput
+    $owner: String
+  ) {
+    onCreateClass(filter: $filter, owner: $owner) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateClass = /* GraphQL */ `
+  subscription OnUpdateClass(
+    $filter: ModelSubscriptionClassFilterInput
+    $owner: String
+  ) {
+    onUpdateClass(filter: $filter, owner: $owner) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteClass = /* GraphQL */ `
+  subscription OnDeleteClass(
+    $filter: ModelSubscriptionClassFilterInput
+    $owner: String
+  ) {
+    onDeleteClass(filter: $filter, owner: $owner) {
+      id
+      className
+      students {
+        items {
+          id
+          studentId
+          classId
+          createdAt
+          updatedAt
+          owner
+        }
+        nextToken
+      }
+      user
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onCreateClassStudent = /* GraphQL */ `
+  subscription OnCreateClassStudent(
+    $filter: ModelSubscriptionClassStudentFilterInput
+    $owner: String
+  ) {
+    onCreateClassStudent(filter: $filter, owner: $owner) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateClassStudent = /* GraphQL */ `
+  subscription OnUpdateClassStudent(
+    $filter: ModelSubscriptionClassStudentFilterInput
+    $owner: String
+  ) {
+    onUpdateClassStudent(filter: $filter, owner: $owner) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteClassStudent = /* GraphQL */ `
+  subscription OnDeleteClassStudent(
+    $filter: ModelSubscriptionClassStudentFilterInput
+    $owner: String
+  ) {
+    onDeleteClassStudent(filter: $filter, owner: $owner) {
+      id
+      studentId
+      classId
+      student {
+        id
+        first_name
+        last_name
+        grade
+        user
+        classes {
+          nextToken
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      class {
+        id
+        className
+        students {
+          nextToken
+        }
+        user
+        createdAt
+        updatedAt
+        owner
+      }
+      createdAt
+      updatedAt
+      owner
     }
   }
 `;
